9150187adba3f5c3218af1ef4adc8a2f0eeacf2a
freestyle09 <jozefrzadkosz09@gmail.com>
2019-03-18 17:46:47 +0100

    First configuration && render object keys

--- src/components/TreeView.tsx
+++ src/components/TreeView.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import { IRuleResult } from '@stoplight/spectral';
 
-export interface TreeViewProps extends React.HTMLProps<HTMLUListElement> { spec: any; diagnostics: IRuleResult[]; }
+export interface TreeViewProps extends React.HTMLProps<HTMLUListElement> {
+  spec: any;
+  diagnostics: IRuleResult[];
+}
 
 export function TreeView({ spec, diagnostics, ...restProps }: TreeViewProps) {
+  const keys: string[] = [...Object.keys(spec)];
 
-  // put you solution here
-
-  return <ul {...restProps} />;
+  return (
+    <ul {...restProps}>
+      {keys.map(el => (
+        <li key={keys.indexOf(el)}>{el}</li>
+      ))}
+    </ul>
+  );
 }
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,18 +3,12 @@ import { TreeView } from '../components/TreeView';
 import { diagnose } from '../util/linting';
 
 export class App extends React.Component<{}, { spec: any }> {
-
   constructor(props: {}) {
     super(props);
     this.state = { spec: require('../../spec/petstore.oas2.json') };
   }
 
   render() {
-    return (
-      <TreeView
-        spec={this.state.spec}
-        diagnostics={diagnose(this.state.spec)} />
-    );
+    return <TreeView spec={this.state.spec} diagnostics={diagnose(this.state.spec)} />;
   }
-
 }
