81c451182661665764130d3465a3b17a7dc1e314
freestyle09 <jozefrzadkosz09@gmail.com>
2019-03-18 20:14:58 +0100

    finished nested arrays

--- src/components/TreeView.tsx
+++ src/components/TreeView.tsx
@@ -7,13 +7,69 @@ export interface TreeViewProps extends React.HTMLProps<HTMLUListElement> {
 }
 
 export function TreeView({ spec, diagnostics, ...restProps }: TreeViewProps) {
-  const keys: string[] = [...Object.keys(spec)];
-  console.log('Checking connection');
   return (
     <ul {...restProps}>
-      {keys.map(el => (
-        <li key={keys.indexOf(el)}>{el}</li>
-      ))}
+      {Object.keys(spec).map((el, index) => {
+        return (
+          <React.Fragment key={index}>
+            <li>
+              {el}
+              {spec[el].constructor === Array && <Arrays array={spec[el]} />}
+            </li>
+          </React.Fragment>
+        );
+      })}
     </ul>
   );
+}
+
+interface Arrays {
+  array: any[];
+}
+
+class Arrays extends React.Component<any, Arrays> {
+  render() {
+    return (
+      <ul>
+        {this.props.array.map((el: any, index: number) => {
+          if (el.constructor === Object) {
+            return (
+              <li key={index}>
+                {index}
+                <NestedArray array={el} />
+              </li>
+            );
+          }
+        })}
+      </ul>
+    );
+  }
+}
+
+interface NestedArray {
+  array: any[];
+}
+
+class NestedArray extends React.Component<any, NestedArray> {
+  render() {
+    console.log(this.props.array);
+    return (
+      <ul>
+        {Object.keys(this.props.array).map((el: any, index: number) => {
+          return (
+            <li key={index}>
+              {el}
+              {this.props.array[el].constructor === Object && (
+                <ul>
+                  {Object.keys(this.props.array[el]).map((el: any, index: number) => {
+                    return <li key={index}>{el}</li>;
+                  })}
+                </ul>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 }
