fb9e7ea10d1254a76812e3b3b74b3bb7484c6baf
freestyle09 <jozefrzadkosz09@gmail.com>
2019-03-18 21:31:51 +0100

    done refactor of nested lists

--- src/components/TreeView.tsx
+++ src/components/TreeView.tsx
@@ -14,9 +14,7 @@ export function TreeView({ spec, diagnostics, ...restProps }: TreeViewProps) {
           <React.Fragment key={index}>
             <li>
               {el}
-              {(spec[el].constructor === Array || spec[el].constructor === Object) && (
-                <FirstNestedList array={spec[el]} />
-              )}
+              {(spec[el].constructor === Array || spec[el].constructor === Object) && <NestedList array={spec[el]} />}
             </li>
           </React.Fragment>
         );
@@ -25,11 +23,7 @@ export function TreeView({ spec, diagnostics, ...restProps }: TreeViewProps) {
   );
 }
 
-interface FirstNestedList {
-  array: any;
-}
-
-class FirstNestedList extends React.Component<any, FirstNestedList> {
+class NestedList extends React.Component<any> {
   render() {
     return (
       <ul>
@@ -37,25 +31,9 @@ class FirstNestedList extends React.Component<any, FirstNestedList> {
           return (
             <li key={index}>
               {el}
-              {this.props.array[el].constructor === Object && <SecondNestedList array={this.props.array[el]} />}
+              {this.props.array[el].constructor === Object && <NestedList array={this.props.array[el]} />}
             </li>
           );
-        })}
-      </ul>
-    );
-  }
-}
-
-interface SecondNestedList {
-  array: any[];
-}
-
-class SecondNestedList extends React.Component<any, SecondNestedList> {
-  render() {
-    return (
-      <ul>
-        {Object.keys(this.props.array).map((el: string, index: number) => {
-          return <li key={index}>{el}</li>;
         })}
       </ul>
     );
